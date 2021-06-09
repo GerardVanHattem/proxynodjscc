0f0ef3fe976c1b829d9c7888cc85e80d69ba8453
GerardProgrammeert <48645416+GerardProgrammeert@users.noreply.github.com>
2021-06-09 19:18:18 +0200

    adjust

--- app.js
+++ app.js
@@ -149,13 +149,10 @@ const port = process.env.port || 3000;
 
 //https://auth0.com/blog/create-a-simple-and-stylish-node-express-app/
 
-app.use(function (req, res, next) {
-    //Enabling CORS
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-    res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With", "Content-Type", "Accept", "x-client-key", "x-client-token", "x-client-secret", "Authorization");
-      next();
-    
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next();
 });
 
 
