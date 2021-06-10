79e54b0d3467a450908c922db7c4643d8d318ede
GerardProgrammeert <48645416+GerardProgrammeert@users.noreply.github.com>
2021-06-10 15:36:01 +0200

    jk

--- app.js
+++ app.js
@@ -58,6 +58,14 @@ var corsOptions = {
 
 app.use(cors('*'))
 
+app.use(function(req, res, next) {
+    if((!req.secure) && (req.get('X-Forwarded-Proto') !== 'https')) {
+        res.redirect('https://' + req.get('Host') + req.url);
+    }
+    else
+        next();
+});
+
 app.use(session({
     cookie: {   maxAge: 86400000 },
     store: new MemoryStore({
