5b5741e1ba786ab2cc3e90a469c9a65a8d542afe
GerardProgrammeert <48645416+GerardProgrammeert@users.noreply.github.com>
2021-07-22 12:25:37 +0200

    update cors

--- app.js
+++ app.js
@@ -69,7 +69,7 @@ const middlewareOptions = {
    }
 }
 
-server.use(function(req, res, next) {
+app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
