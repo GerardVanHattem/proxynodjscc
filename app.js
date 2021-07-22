a4efe0f7b61be21046c9ad930bdbc90b7a44d094
GerardProgrammeert <48645416+GerardProgrammeert@users.noreply.github.com>
2021-07-22 12:29:43 +0200

    update cors

--- app.js
+++ app.js
@@ -70,7 +70,7 @@ const middlewareOptions = {
 }
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://master.d23zxthy4ykh0n.amplifyapp.com')
   res.header('Access-Control-Allow-Credentials', true)
   res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
